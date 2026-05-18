bb2c95a8b949922f53a2be72d22ec807257a07b7
shubhashree500 <shreeshubha98@gmail.com>
2026-05-18 18:40:55 +0530

    check middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,5 @@
 import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
 
-export function middleware(request: NextRequest) {
-  const host = request.headers.get("host") || ""
-
-  if (host.startsWith("www.")) {
-    const url = request.nextUrl.clone()
-    url.host = host.replace("www.", "")
-    return NextResponse.redirect(url, 308)
-  }
-
+export function middleware() {
   return NextResponse.next()
 }
